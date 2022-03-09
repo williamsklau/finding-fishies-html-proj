cb091e9f341644a8dbbe638ce3da779d80dfc5ef
WLau <williamsklau@gmail.com>
2022-03-09 13:38:56 -0800

    Sprint A-1 Completed
    Includes:
    -sprint outlines
    -HTML, CSS, JS files
    -canvas set to an image
    -push a fish image onto canvas when game start

---
+++ main.js
@@ -0,0 +1,7 @@
+function startGame(){
+	var canvas = document.getElementById('background');
+	var ctx = canvas.getContext('2d');
+	var img = document.createElement('img');
+	img.src = "fish.png";
+	ctx.drawImage(img, 100, 100, 840, 230);
+}
